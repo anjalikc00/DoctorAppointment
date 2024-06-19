8ec64a2e4f94e4ee0569c3cdc0d25a1031dd7273
AnjaliKhatrichhetri <chhetrianjali31@gmail.com>
2024-06-19 20:42:27 +0545

    db connected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import{BrowserRouter,Route,Routes}from "react-router-dom";
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 
 function App() {
   return (
-    <div className="App">
-  <h1>hello</h1>
-    </div>
+    <BrowserRouter>
+    
+      <Routes>
+        <Route path="/login"element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+   
+    </BrowserRouter>
   );
 }
 
